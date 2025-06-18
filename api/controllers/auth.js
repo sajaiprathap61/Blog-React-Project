c4b51d7a306425418eaf71baeb909c5c739bc03b
sajaiprathap61 <sajaiprathap61@gmail.com>
2025-06-18 18:18:19 +0530

    [Dev] Implemented the server side architecture for the basic routes

---
+++ api/controllers/auth.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+  res.json("from register");
+};
+
+export const login = (req, res) => {
+  res.json("login");
+};
+
+export const logout = (req, res) => {
+  res.json("logout");
+};
---
+++ api/controllers/users.js
@@ -0,0 +1,3 @@
+export const getUser = (req, res) => {
+  res.json("from user controller");
+};
